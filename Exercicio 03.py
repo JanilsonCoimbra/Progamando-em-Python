a3f5aae56228cbf7249bf70deb9d2b02eddce809
JanilsonCoimbra <janilson.coimbra@gmail.com>
2021-02-06 15:15:02 -0300

    Exercicio 03 e 04

---
+++ Exercicio 03.py
@@ -0,0 +1,8 @@
+print('Vamos criar um programa que exiba a soma de 2 valores')
+print('Vamos começar!')
+valor1 = int(input('Comece digitando o primeiro valor: '))
+print('Muito bem!')
+valor2 = int(input('Agora digite o segundo valor: '))
+print('Otimo, agora vamos somar...')
+resultado = valor1 + valor2
+print('A soma de {} com o {} é {}'.format(valor1, valor2, resultado))
