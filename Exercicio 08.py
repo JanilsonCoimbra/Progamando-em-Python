c69386e0e940b56d058de88ec0f2a85c71ea349d
JanilsonCoimbra <janilson.coimbra@gmail.com>
2021-02-07 19:28:41 -0300

    Esse programa converte metros em centimentros

---
+++ Exercicio 08.py
@@ -0,0 +1,3 @@
+print('Esse programa converte metros em centimentros')
+valor = int(input('Digite aqui o valor em centimetros: '))
+print('{} metros equivale a {} centimetros'.format(valor, valor*100))
