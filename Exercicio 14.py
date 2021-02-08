521cd4a8466388bccb986018e211e26c0003fe71
JanilsonCoimbra <janilson.coimbra@gmail.com>
2021-02-07 21:53:58 -0300

    Programa que convert uma temperatura em Cº para ºF

---
+++ Exercicio 14.py
@@ -0,0 +1,4 @@
+print('Programa que convert uma temperatura em Cº para ºF')
+c = float(input('Digite qual a temperatura em C°: '))
+f= ((9*c)/5)+32
+print('A temperatura de {} C° equivale a {} F° '.format(c, f))
