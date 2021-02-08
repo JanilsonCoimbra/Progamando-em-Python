ba48ec63d51ae4a210d07509231ed17cc0172789
JanilsonCoimbra <janilson.coimbra@gmail.com>
2021-02-07 21:00:06 -0300

    Esse programa converte metros em todas as unidades de medidas

--- Exercicio 07.py
+++ Exercicio 07.py
@@ -1,9 +1,9 @@
 print('Aqui é calculado as notas do aluno para obter a média ')
-nota1 = int(input('Digite a nota da primeira unidade: '))
-nota2 = int(input('Digite a nota da segunda unidade: '))
-nota3 = int(input('Digite a nota da terceira unidade: '))
-nota4 = int(input('Digite a nota da quarta unidade: '))
+nota1 = float(input('Digite a nota da primeira unidade: '))
+nota2 = float(input('Digite a nota da segunda unidade: '))
+nota3 = float(input('Digite a nota da terceira unidade: '))
+nota4 = float(input('Digite a nota da quarta unidade: '))
 
 print('Agora vamos ver qual a média do aluno!')
 
-print('A média foi: {}'.format((nota1+nota2+nota3+nota4)/4) )
+print('A média foi: {:0.1f}'.format((nota1+nota2+nota3+nota4)/4) )
--- Exercicio 08.py
+++ Exercicio 08.py
@@ -1,3 +1,9 @@
 print('Esse programa converte metros em centimentros')
-valor = int(input('Digite aqui o valor em centimetros: '))
+valor = float(input('Digite aqui o valor em Metros: '))
+
+print('{} metros equivale a {} kilometros'.format(valor, valor/1000))
+print('{} metros equivale a {} hectometros'.format(valor, valor/100))
+print('{} metros equivale a {} decametros'.format(valor,valor/10))
+print('{} metros equivale a {} decimetros'.format(valor, valor*10))
 print('{} metros equivale a {} centimetros'.format(valor, valor*100))
+print('{} metros equivale a {} milimetros'.format(valor, valor*1000))
