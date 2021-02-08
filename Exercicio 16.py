b8813d74678ac58ce0eed81442ae66cae97c015b
JanilsonCoimbra <janilson.coimbra@gmail.com>
2021-02-08 20:50:38 -0300

    Programa em python que mostra o numero inteiro de um float

--- Exercicio 16.py
+++ Exercicio 16.py
@@ -1,4 +1,6 @@
 from math import floor
+from time import sleep
 print('Programa em python que mostra o numero inteiro de um float')
 num = float(input('Digite o valor de entrada: '))
 print('O numero inteiro de {} é {}'.format(num, floor(num)))
+sleep(10)
