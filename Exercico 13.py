22c4566c1e6a5ec0aba1114352cfc347018efeb0
JanilsonCoimbra <janilson.coimbra@gmail.com>
2021-02-07 21:21:38 -0300

    Programa que da um almento de 15% no salario de um funcionário

--- Exercico 13.py
+++
@@ -1,3 +0,0 @@
-print('Programa que da um almento de 15% no salario de um funcionário')
-salario = float(input('Qual o salario: '))
-print('O salário atual é {:0.2f} reais e com um aumento de 15% fica {:0.2f} reais'.format(salario, salario+(salario*15/100)))
