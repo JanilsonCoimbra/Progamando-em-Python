74366c4da1be1787699499a86a7ff3eb4035b04e
JanilsonCoimbra <janilson.coimbra@gmail.com>
2021-02-07 21:14:32 -0300

    Esse codigo exibe a cotação do dólar

--- Exercicio 10.py
+++ Exercicio 10.py
@@ -1,3 +1,3 @@
 print('Esse codigo exibe a cotação do dólar')
 valor = float(input('Digite um valor em reais: '))
-print('{} reais equivale a {} dólar.'.format(valor, valor*3.27))
+print('R${} reais equivale a ${:0.2f} dólar.'.format(valor, valor/3.27))
