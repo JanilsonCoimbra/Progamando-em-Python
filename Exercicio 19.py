299074768dc22c74d105a0cdca3b68c59562637f
JanilsonCoimbra <janilson.coimbra@gmail.com>
2021-02-09 21:03:39 -0300

    Programa que escolhe entre 4 alunos 1 para apagar o quadro

---
+++ Exercicio 19.py
@@ -0,0 +1,9 @@
+from random import choice
+print('Programa que escolhe entre 4 alunos 1 para apagar o quadro')
+a1 = str(input('Primeiro aluno: '))
+a2 = str(input('Segundo aluno: '))
+a3 = str(input('Terceiro aluno: '))
+a4 = str(input('Quarto aluno: '))
+lista = [a1, a2, a3, a4]
+escolhido = choice(lista)
+print('O aluno escolhido foi {}'.format(escolhido))
