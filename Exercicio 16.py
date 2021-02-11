7eb9c98404bd0122a5bbbe27d40f02737c19a50b
JanilsonCoimbra <janilson.coimbra@gmail.com>
2021-02-10 23:31:45 -0300

    Programa que ler a velocidade de um carro e aplica multa

--- Exercicio 16.py
+++ Exercicio 16.py
@@ -2,5 +2,5 @@ from math import trunc
 from time import sleep
 print('Programa em python que mostra o numero inteiro de um float')
 num = float(input('Digite o valor de entrada: '))
-print('O numero inteiro de {} é {}'.format(num, trunc(num)))
+print('O numero inteiro de {} é {}'.format(num, trunc(num))) # trunc exibe o valor inteiro do numero
 sleep(10)
--- Exercicio 19.py
+++ Exercicio 19.py
@@ -5,5 +5,5 @@ a2 = str(input('Segundo aluno: '))
 a3 = str(input('Terceiro aluno: '))
 a4 = str(input('Quarto aluno: '))
 lista = [a1, a2, a3, a4]
-escolhido = choice(lista)
+escolhido = choice(lista) #Choice escolhe um valor aleatorio de umalista
 print('O aluno escolhido foi {}'.format(escolhido))
