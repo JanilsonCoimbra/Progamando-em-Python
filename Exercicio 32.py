237cbde2b2d81a9a5d4f627939c54b319635d070
JanilsonCoimbra <janilson.coimbra@gmail.com>
2021-02-11 15:26:06 -0300

    Programa que ver se o ano é bixesto

--- Exercicio 32.py
+++ Exercicio 32.py
@@ -1,8 +1,11 @@
+from datetime import date
 print('Programa que ver se o ano é bixesto')
 ano = int(input('Digite um ANO: '))
-b = ano % 4
-bb = ano % 100
-if b == 0 and bb != 0:
+if ano == 0:
+    ano = date.today().year
+    print('Estamo no ano de {}'.format(ano))
+
+if ano % 4 == 0 and ano % 100 != 0 or ano % 400 == 0:
     print('O ano {} é um ano bixesto'.format(ano))
     print(bb)
 else:
