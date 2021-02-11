9f7286aa6e4c917e71342a1fdacde7b92999b6a4
JanilsonCoimbra <janilson.coimbra@gmail.com>
2021-02-11 15:25:01 -0300

    Computador escolhe numero de 0 a 5 e usuario tenta adivinhar

--- Exercicio 28.py
+++ Exercicio 28.py
@@ -1,10 +1,10 @@
 from random import randint
-print('Computador escolhe numero de 0 a 99 e usuario tenta adivinhar')
-numero = int(randint(0,99))
+print('Computador escolhe numero de 0 a 5 e usuario tenta adivinhar')
+numero = int(randint(0,5))
 print(numero)
-
+print('-=-'*20)
 escolha = int(input('Escolha um numero de 0 a 5 para jogar...'))
-
+print('-=-'*20)
 if numero == escolha:
     print('PARABÉNS VOCÊ GANHOU O PRÊMIO!')
 else:
