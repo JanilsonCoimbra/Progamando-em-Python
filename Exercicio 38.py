b3bfb55d70eff43e94653661625270082f079bbb
JanilsonCoimbra <janilson.coimbra@gmail.com>
2021-02-13 00:03:48 -0300

    Programa que ler 2 valores e diz qual o maior

---
+++ Exercicio 38.py
@@ -0,0 +1,7 @@
+print('Programa que ler 2 valores e diz qual o maior')
+v1 = int(input('Valor 01 :'))
+v2 = int(input('Valor 02 :'))
+if v1 > v2:
+    print('Entre {} e {} o maior valor é {}'.format(v1, v2, v1))
+else:
+    print('Entre {} e {} o maior valor é {}'.format(v1, v2, v2))
