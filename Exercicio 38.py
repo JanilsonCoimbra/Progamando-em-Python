041a51180f8115e749f045ed1960730115954356
JanilsonCoimbra <janilson.coimbra@gmail.com>
2021-02-14 00:08:44 -0300

    Programa que ler 2 valores e diz qual o maior

--- Exercicio 38.py
+++ Exercicio 38.py
@@ -3,5 +3,7 @@ v1 = int(input('Valor 01 :'))
 v2 = int(input('Valor 02 :'))
 if v1 > v2:
     print('Entre {} e {} o maior valor é {}'.format(v1, v2, v1))
+elif v1 == v2:
+    print('Os numeros são iguais!')
 else:
     print('Entre {} e {} o maior valor é {}'.format(v1, v2, v2))
