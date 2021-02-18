0e85a7eb0fc0fe8e88e255e933ef69eebdcfc489
JanilsonCoimbra <janilson.coimbra@gmail.com>
2021-02-18 18:10:01 -0300

    10 primeiros termos de uma Progressão aritimética

--- Exercicio 51.py
+++ Exercicio 51.py
@@ -4,6 +4,7 @@ print('\033[33m 10 TERMOS DE UMA PA\033[m')
 print('='*22)
 n1 = int(input('Digite o primeiro termo :'))
 r = int(input('Razão :'))
-for c in range(n1, 11, r):
+dec = n1 + (10 - 1) * r
+for c in range(n1, dec, r):
     print(c, end=' -> ')
 print('FIM')
