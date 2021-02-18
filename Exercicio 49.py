a5f5cb3b79072080e51026124a96cea43867d266
JanilsonCoimbra <janilson.coimbra@gmail.com>
2021-02-18 17:33:22 -0300

    Tabuada simplificada ultilizando estrutura FOR

--- Exercicio 49.py
+++ Exercicio 49.py
@@ -1,5 +1,4 @@
 #Tabuada simplificada ultilizando estrutura FOR
 num = int(input('Digite um numero para ver sua tabuada :'))
 for c in range(1, 11):
-    result = num * c
-    print('{} x {} = {}'.format(num, c, result))
+    print('{} x {} = {}'.format(num, c, num * c))
