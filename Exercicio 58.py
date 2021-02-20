84b706f378356246a2fff5887debccaa648a8e07
JanilsonCoimbra <janilson.coimbra@gmail.com>
2021-02-20 00:35:45 -0300

    Computador gera numero aleatorio para usuario acertar numero

--- Exercicio 58.py
+++ Exercicio 58.py
@@ -1,16 +1,16 @@
 #Computador gera numero aleatorio para usuario acertar numero
 from random import randint
-print('Sou seu computador...\nAcabei de pensar um numero entre 0 e 10\nSerá que você consegue adivinhar qual foi?')
+print('Sou seu computador...\nAcabei de pensar um numero entre 0 e 1000\nSerá que você consegue adivinhar qual foi?')
 palpite = int(input('Qual o seu palpete? '))
-pc = randint(0, 10000)
+pc = randint(0, 1000)
 c = 1
 while not palpite == pc:
     if palpite < pc:
-        print('MAIS... tente denovo!')
+        print('\033[33mMAIS...\033[m tente denovo!')
         palpite = int(input('Qual o proximo palpite? '))
         c += 1
     else:
-        print('MENOS... Tente denovo!')
+        print('\033[33mMENOS...\033[m Tente denovo!')
         palpite = int(input('Qual seu proximo palpite? '))
         c += 1
 
