28461255aa036e8f0041c45e657337a686485f46
JanilsonCoimbra <janilson.coimbra@gmail.com>
2021-02-19 23:30:35 -0300

    Programa de validação de dados

--- Exercicio 57.py
+++ Exercicio 57.py
@@ -1,1 +1,8 @@
-#Programa que só aceita o valor M/F
+#Programa de validação de dados
+sexo = str(input('Sexo [M/F]')).strip().lower()[0]
+while not sexo in 'mf':
+    sexo = str(input('Sexo invalido digite o Sexo correto [M/F]')).strip().lower()[0]
+print('O sexo registrado foi {} '.format(sexo.upper()))
+
+
+
