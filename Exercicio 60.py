d0eee653713b61cdb9aa7fa962c09df867d90cea
JanilsonCoimbra <janilson.coimbra@gmail.com>
2021-02-26 16:29:57 -0300

    Exercicio que faz umap rogressão aritimetica

---
+++ Exercicio 60.py
@@ -0,0 +1,16 @@
+#Exercicio que faz umap rogressão aritimetica
+primeiro = int(input('Primeiro termo :'))
+razao = int(input('Razão :'))
+termo = primeiro
+cont = 1
+while cont <= 10:
+    print(termo, end=' ')
+    termo = termo + razao
+    cont = cont + 1
+print('FIM')
+
+
+
+'''for c in range(primeiro, ultimo+razao, razao):
+    print('{} ->'.format(c), end=' ')
+print('FIM')'''
