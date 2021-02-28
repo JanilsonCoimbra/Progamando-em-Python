56d930fc01810f84ec8a8966ed77c0707b89c2b5
JanilsonCoimbra <janilson.coimbra@gmail.com>
2021-02-27 23:54:21 -0300

    Digite um numero até o usuario digitar 999 para parar

--- Exercicio 64.py
+++ Exercicio 64.py
@@ -1,8 +1,8 @@
 #Digite um numero até o usuario digitar 999 para parar
 n = 0
 soma = 0
+n = int(input('Digite um numero [999 para parar]'))
 while not n == 999:
+    soma += n
     n = int(input('Digite um numero [999 para parar]'))
-    if n != 999:
-        soma += n
 print(soma)
