8b4532c3dae1059e5c9ea0d32da7062e0d60d21a
JanilsonCoimbra <janilson.coimbra@gmail.com>
2021-02-27 23:50:31 -0300

    Digite um numero até o usuario digiatr 999 para parar

---
+++ Exercicio 64.py
@@ -0,0 +1,8 @@
+#Digite um numero até o usuario digiatr 999 para parar
+n = 0
+soma = 0
+while not n == 999:
+    n = int(input('Digite um numero [999 para parar]'))
+    if n != 999:
+        soma += n
+print(soma)
