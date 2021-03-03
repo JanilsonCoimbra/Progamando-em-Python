d24061a9c2472dc135d7e732dd937297e168687f
JanilsonCoimbra <janilson.coimbra@gmail.com>
2021-03-03 01:25:09 -0300

    Simulador de caixa eletrônico

--- Exercicio 71.py
+++ Exercicio 71.py
@@ -6,20 +6,18 @@ while True:
     c = (valor % 1000 // 1) - d - u
     m = (valor % 100000 // 1) - u - d - c
     ced1 = u / 1
+    ced10 = 0
+    ced50 = 0
     if d >= 50:
-        ced50 = 1
-        if c >= 100:
-            ced50 += (c / 50)
-            if m >= 1000:
-                ced50 += (m / 50)
-        ced10 = (d - 50) / 10
+        ced50 += 1
+        ced10 += (d - 50) / 10
     else:
-        ced50 = 0
-        if c >= 100:
-            ced50 += (c / 50)
-            if m >= 1000:
-                ced50 += (m / 50)
-        ced10 = d / 10
+        ced10 += d / 10
+    if c >= 100:
+        ced50 += (c / 50)
+    if m >= 1000:
+        ced50 += (m / 50)
+
     print(f'{int(ced1)} cedulas de R$1,00 real')
     print(f'{int(ced10)} cedulas de 10,00 reais')
     print(f'{int(ced50)} cedulas de R$50,00 reais')
