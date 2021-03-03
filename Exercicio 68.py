43f9609c3810a9c6ea314688330ad1713a6ae88b
JanilsonCoimbra <janilson.coimbra@gmail.com>
2021-03-02 23:11:36 -0300

    Jogo de PAR ou IMPAR, usuario VS PC

--- Exercicio 68.py
+++ Exercicio 68.py
@@ -1,29 +1,36 @@
 #Jogo de PAR ou IMPAR, usuario VS PC
-from random import choice, randint
+from random import randint
 from time import sleep
-pc = ''
-numero = 0
-vitorias = 0
-derrotas = 0
+ganhou = perdeu = 0
 while True:
-    valor = randint(0, 10)
-    escolha = int(input('Digite [1] para escolher ÍMPAR ou [0] para PAR :'))
-    if escolha == 1:
+    pc = randint(0, 10)
+    sleep(1)
+    v = int(input('Diga um valor :'))
+    pi = str(input('Par ou Ímpar? [P/I]')).strip().lower()[0]
+    if not pi in 'piPI':
+        while True:
+            pi = str(input('Par ou Ímpar? [P/I]')).strip().lower()[0]
+            if pi in 'PIpi':
+                break
+    r = (pc + v) % 2
+    if pi in 'pP':
+        a = 0
+    elif pi in 'iI':
+        a = 1
+    print('-=-' * 18)
+    print(f'Você jogou {v} e o PC jogou {pc} no total deu {v + pc} que é {"PAR" if r == 0 else "ÍMPAR"}')
+    print('-=-' * 18)
+    if a == r:
         sleep(1)
-        print(f'\033[33mVocê escolheu ÍMPAR\033[m')
-    elif escolha == 0:
-        print('\033[33mVocê escollheu PAR\033[m')
-    numero = int(input('Digite um numero :'))
-    res = (valor + numero) % 2
-    if res == escolha:
-        print(f'\033[33mO PC escolheu {valor} e você {numero}\033[m')
-        sleep(1)
-        print('PARABÉNS, VOCÊ GANHOU!')
-        vitorias += 1
+        print('\033[33mVOCÊ GANHOU\033[m')
+        ganhou += 1
     else:
-        print(f'\033[33mO PC escolheu {valor} e você {numero}\033[m')
         sleep(1)
-        print('AHAHHA EU GANHEI!')
-        derrotas += 1
+        print('\033[33mPC GANHOU\033[m')
+        perdeu += 1
         break
-print(f'Você Ganhou\033[33m {vitorias} \033[m e perdeu \033[33m {derrotas}\033[m')
+sleep(1)
+print(f'GAME OVER \nVocê ganhou {ganhou} e perdeu {perdeu} vez')
+
+
+
