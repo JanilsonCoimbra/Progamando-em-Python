7994691f4330489c119a51e7422615312d6f0856
JanilsonCoimbra <janilson.coimbra@gmail.com>
2021-04-06 20:09:33 -0300

    Função que ler nome de um jogador e seus gols e retorna os dados

--- Exercicio 103.py
+++ Exercicio 103.py
@@ -1,8 +1,16 @@
 #Função que ler nome de um jogador e seus gols e retorna os dados
-def ficha(nome="<desconhecido>", gol=0):
-    n = nome or '<desconhecido>'
-    g = gol or 0
-    print(f'O jogador {n} fez {g} gols')
+def ficha(nome='<desconhecido>', gol=0):
+    n = nome
+    g = gol
+    if g.isnumeric():
+        g = int(g)
+    else:
+        g = 0
+    if n.strip() == '':
+        ficha(gol=g)
+    else:
+        ficha(n, g)
+    print(f'O jogador {nome} fez {g} gols')
 
 
 ficha(str(input('Nome :')), input('Gols :'))
