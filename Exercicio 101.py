76ceacc5727d78c6273f4aba74afbcf9ca1f9739
JanilsonCoimbra <janilson.coimbra@gmail.com>
2021-04-06 20:06:53 -0300

    Função que retorna com base no ano de nascimento se pode votar

--- Exercicio 101.py
+++ Exercicio 101.py
@@ -1,13 +1,13 @@
 #Função que retorna com base no ano de nascimento se pode votar
-from datetime import date
 def voto(nasc):
+    from datetime import date
     ano = date.today().year
     idade = ano - nasc
     if idade < 16:
         print(f'Com {idade} anos: Não é possovél VOTAR ainda!')
     elif 16 <= idade < 18:
         print(f'Com {idade} anos: O VOTO é opcional ')
-    elif 18 <= idade <= 65:
+    elif idade < 65:
         print(f'Com {idade} anos: VOTO é obrigatório!')
     elif idade > 65:
         print(f' Com {idade} anos: O VOTO é opcional')
