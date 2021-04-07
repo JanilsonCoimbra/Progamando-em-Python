65b1c80fb5209ae3c649f51f4dc38fc3d9029374
JanilsonCoimbra <janilson.coimbra@gmail.com>
2021-04-06 23:45:14 -0300

    Função que só aceita entrada de numeros e trata erros de usuario

--- Exercicio 104.py
+++ Exercicio 104.py
@@ -1,14 +1,14 @@
 #Função que só aceita entrada de numeros e trata erros de usuario
-def LeiaInt(num):
-    print(f'\033[33mVocê digitou o numero {num}\033[m')
+def LeiaInt(msg):
+    valor = 0
+    while True:
+        n = str(input(msg))
+        print(f'\033[33mVocê digitou o numero {n}\033[m')
+        if n == 'sair':
+            break
+        if n.isnumeric():
+            valor = int(n)
+        else:
+            print('\033[94mNÃO É UM NUMERO NUMERICO VALIDO\033[m')
 
-
-while True:
-    n = input('Digite um numero ou sair :')
-    if n == 'sair':
-        break
-    if n.isnumeric():
-        LeiaInt(int(n))
-    else:
-        print('\033[94mNÃO É UM NUMERO NUMERICO VALIDO\033[m')
-
+LeiaInt('Digite um numero ou sair :')
