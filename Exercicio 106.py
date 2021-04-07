b4bc76145a8a7c6f6e5adac2b106a134621d2eb3
JanilsonCoimbra <janilson.coimbra@gmail.com>
2021-04-07 00:57:11 -0300

    Programa que acessa sistema de ajuda em python

--- Exercicio 106.py
+++ Exercicio 106.py
@@ -1,17 +1,27 @@
 #Programa que acessa sistema de ajuda em python
+from time import sleep
+cores = (   '\033[m', #neutro
+            '\033[1;7;33m', #Amarelo
+            '\033[7;94m', #Azul
+            '\033[7;97m' #Branco
+            )
+def titulo(msg, cor=0):
+    print(cores[cor], end='')
+    print('~' * (len(msg) + 4))
+    print(' ', msg)
+    print('~' * (len(msg) + 4))
+    print(cores[0], end='')
+def ajuda(com):
+    help(com)
+
+
 while True:
-    print('\033[1;7;33m~' * 25)
-    print('SISTEMA DE AJUDA PYTHON')
-    print('~' * 25)
-    f = str(input('\033[mFunção ou Biblioteca >')).lower()
-    if f == 'fim':
-        print('\033[1;7;95m~' * 12)
-        print('  Até logo')
-        print('~' * 12)
+    titulo('SISTEMA DE AJUDA HELP', 1)
+    com = str(input('Digite um comando: '))
+    sleep(1)
+    if com.lower() == 'sair':
+        titulo('Até logo!', 2)
         break
-    tam = len(f)
-    print('\033[7;94m~' * (30 + tam))
-    print(f'ACESSANDO MANUAL DO COMANDO {f}')
-    print('~' * (30 + tam))
-    print('\033[7;97m ')
-    print(f'{help(f)}')
+    else:
+        print(cores[3], end='')
+        ajuda(com)
