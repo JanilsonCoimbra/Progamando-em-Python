6f733e8ce3cb9a059c5d2bc209503e900d1bd597
JanilsonCoimbra <janilson.coimbra@gmail.com>
2021-04-07 00:04:54 -0300

    Função que recebe notas dos alunos e retorna uma analise e sua situação

--- Exercicio 105.py
+++ Exercicio 105.py
@@ -5,21 +5,11 @@ def notas(*n, sit=False):
     :param sit: Valor bool para mostrar se a situação do aluno é boa ou ruim
     :return: retorna um dicionário com informações
     """
-    maior = menor = cont = TotSomaNotas = 0
     aluno = dict()
-    aluno["total"] = len(n)
-    for notas in n:
-        if cont == 0:
-            menor = notas
-        if notas > maior:
-            maior = notas
-        if menor > notas:
-            menor = notas
-        TotSomaNotas += notas
-        cont += 1
-    aluno["MaiorNota"] = maior
-    aluno["MenorNota"] = menor
-    aluno["media"] = TotSomaNotas / aluno["total"]
+    aluno["Total"] = len(n)
+    aluno["MaiorNota"] = max(n)
+    aluno["MenorNota"] = min(n)
+    aluno["media"] = sum(n)/len(n)
     if sit == True:
         if aluno["media"] >= 7:
             aluno["sit"] = 'BOM'
@@ -28,4 +18,4 @@ def notas(*n, sit=False):
     print(aluno)
 
 
-notas(10 ,3 ,10 ,7 ,6 ,8 ,4 ,10, sit=True )
+notas(10, 3, 10, 7, sit=True)
